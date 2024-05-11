bc17d6beb1ae94caaee914e32cbeef73aa093a2b
Nathal25 <nathalia.ortiz.granada@correounivalle.edu.co>
2024-05-11 18:11:38 -0500

    Permite arrastrar la figura, se añadió la clase que permite arrastrar cualquier nodo.

--- src/main/java/com/example/navalbattle/controller/GameController.java
+++ src/main/java/com/example/navalbattle/controller/GameController.java
@@ -1,8 +1,31 @@
 package com.example.navalbattle.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
 public class GameController {
 
-    public void initialize(){
+    @FXML
+    private GridPane gridPlayer;
 
+    @FXML
+    private HBox hbxHead;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Pane paneBaseA;
+    @FXML
+    private Pane paneBaseB;
+    private StartController startController;
+
+    public void initialize(){
+        gridPlayer = new GridPane();
+        startController = new StartController();
+        gridPlayer=startController.getBoardPlayer1();
     }
 }
--- src/main/java/com/example/navalbattle/controller/StartController.java
+++ src/main/java/com/example/navalbattle/controller/StartController.java
@@ -1,19 +1,19 @@
 package com.example.navalbattle.controller;
 
+import com.example.navalbattle.model.DraggableMaker;
 import com.example.navalbattle.model.ImagesCreator;
 import com.example.navalbattle.view.GameStage;
 import com.example.navalbattle.view.StartStage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.awt.event.MouseAdapter;
 import java.io.IOException;
-
-import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
-import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionListener;
 
 /**
  * This class generates the first stage designed to put
@@ -25,6 +25,20 @@ public class StartController extends Stage {
     @FXML
     private GridPane boardPlayer1;
 
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private HBox hbxHead;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private Rectangle shpSQ;
+
+    DraggableMaker draggableMaker = new DraggableMaker();
+
     private ImagesCreator imagesCreator;
 
     public void initialize(){
@@ -34,28 +48,18 @@ public class StartController extends Stage {
                 boardPlayer1.add(imagesCreator.getImageView(),i,j);
             }
         }
+        draggableMaker.makeDraggable(shpSQ);
 
-    }
+        /*
+        shpSQ.setOnMouseReleased(event -> {
+            // Calcular la celda del GridPane
+            int colIndex = (int) (event.getSceneX() / (gridPane.getWidth() / GridPane.getColumnIndex(shpSQ)));
+            int rowIndex = (int) (event.getSceneY() / (gridPane.getHeight() / GridPane.getRowIndex(shpSQ)));
 
-    void mouseDragged(){
-        MouseAdapter mouseAdapter = new MouseAdapter() {
-            int distanceX, distanceY;
-            boolean moved = false;
-
-            public void mousePressed(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseReleased(MouseEvent mouseEvent) {
-
-            }
-
-            public void mouseDragged(MouseEvent mouseEvent) {
-
-            }
-        };
-        addMouseListener(mouseAdapter);
-        addMouseMotionListener(mouseAdapter);
+            // Colocar el rectángulo en la celda del GridPane
+            GridPane.setConstraints(shpSQ, colIndex, rowIndex);
+        });
+         */
     }
     @FXML
     void onHandleButtonStartGame(ActionEvent event) throws IOException {
@@ -63,4 +67,7 @@ public class StartController extends Stage {
         StartStage.deleteInstance();
     }
 
+    public GridPane getBoardPlayer1() {
+        return boardPlayer1;
+    }
 }
