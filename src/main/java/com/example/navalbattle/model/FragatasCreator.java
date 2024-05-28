40b7c49751a7cc17f846fb23bdb9eff2ea0ed3b7
Syineth <sarareyes1227@gmail.com>
2024-05-27 22:15:07 -0500

    Se organiza la clase FragatasCreator

--- src/main/java/com/example/navalbattle/model/FragatasCreator.java
+++ src/main/java/com/example/navalbattle/model/FragatasCreator.java
@@ -13,29 +13,39 @@ public class FragatasCreator {
 
     public FragatasCreator() {
         fragata = new Polygon(
-                0,32,//A
-                16,0,//B
-                32,32
+                0, 32,//A
+                16, 0,//B
+                32, 32
         );
-        fragata.setFill(Color.rgb(0,128,128));
-        fragata.setStroke(Color.rgb(0,79,79));
+        fragata.setFill(Color.rgb(0, 128, 128));
+        fragata.setStroke(Color.rgb(0, 79, 79));
         fragata.setStrokeWidth(1);
 
         // Agrega un evento de click al fragata para detectar clics del usuario
         fragata.setOnMouseClicked(this::handleFragatasClick);
     }
+
     public Polygon getFragata() {
         return fragata;
     }
-    public void setFragata(Polygon fragata) {this.fragata = fragata;}
 
-    public double getLayoutX() {return layoutX;}
+    public void setFragata(Polygon fragata) {
+        this.fragata = fragata;
+    }
+
+    public double getLayoutX() {
+        return layoutX;
+    }
+
     public void setLayoutX(double layoutX) {
         this.layoutX = layoutX;
         fragata.setLayoutX(layoutX);
     }
 
-    public double getLayoutY() {return layoutY;}
+    public double getLayoutY() {
+        return layoutY;
+    }
+
     public void setLayoutY(double layoutY) {
         this.layoutY = layoutY;
         fragata.setLayoutY(layoutY);
@@ -44,15 +54,10 @@ public class FragatasCreator {
     // Método para manejar el evento de click el la fragata
     private void handleFragatasClick(javafx.scene.input.MouseEvent mouseEvent) {
         if (mouseEvent.getButton() == MouseButton.SECONDARY) { // botón derecho
-            // Rotar el fragata cuando se hace clic derecho
-            rotateClockwise();
-            System.out.println("Fragata rotado ");
+            // Rotar el submarino cuando se hace clic derecho
+            fragata.getTransforms().add(new javafx.scene.transform.Rotate(90,16,17));
+            System.out.println("Submarino rotado");
         }
-    }
-    // Método para rotar el fragata
-    private void rotateClockwise() {
-        Rotate rotate = new Rotate(90, fragata.getBoundsInLocal().getWidth() / 2, fragata.getBoundsInLocal().getHeight() / 2);
-        fragata.getTransforms().add(rotate);
-    }
 
+    }
 }
